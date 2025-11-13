898fb92853592c2cd416f7a70ec4c414993fdd29
Wiedmolol-bg <al.dimitrov.26@gmail.com>
2025-11-13 21:56:18 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+var videoBonuses = [15];
+var code_num = [3,2,3,3];
+var baseStr = ["","","",""];
+var graalStr = ["","","",""];
+var accNames = ["","","",""];
+var accs = 1;
+var begDates = [[2024, 10, 10],[2021, 2, 31],[2021, 4, 23],[2021, 2, 31]];
+var range = [802,133,292,133];
+var isLiteVersion = true;
+var isAutoLevel = [true,false,false,false];
